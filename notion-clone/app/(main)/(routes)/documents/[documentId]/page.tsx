725cd081019b690ce63eb3adb09ee2b42ab728a2
Rcatarata <Rcatarata@gmail.com>
2023-10-27 22:16:50 -0400

    icon selector

--- notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,63 @@
-import React from 'react'
+'use client'
 
-function DocumentIdPage() {
+import {useMutation, useQuery} from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useMemo } from 'react'
+import dynamic from 'next/dynamic'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Toolbar } from '@/components/Toolbar'
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<'documents'>
+  }
+}
+
+function DocumentIdPage({params}: DocumentIdPageProps) {
+  // const Editor = useMemo(() => dynamic(() => import('@/components/editor'), {ssr: false}), [])
+  
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  })
+
+  const update = useMutation(api.documents.update)
+
+  const onChange = ((content: string) => {
+    update({
+      id: params.documentId, 
+      content
+    })
+  })
+
+  if (document === undefined) {
+    return (
+      <div>
+        {/* <Cover.Skeleton /> */}
+        <div className='md:max-w-3xl lg:max-w-4xl mx-auto mt-10'>
+          <div className='space-y-4 pl-8 pt-4'>
+            <Skeleton className='h-14 w-[50%]' />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div> 
+        </div>
+      </div>
+    )
+  }
+
+  if (document === null) {
+    return <div>Not Found</div>
+  }
+
   return (
-    <div>DocumentIdPage</div>
+    <div className='pb-40'>
+      {/* <Cover url={document.coverImage} /> */}
+      <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+        <Toolbar initialData = {document} />
+        {/* <Editor onChange={onChange} initialContent={document.content} /> */}
+      </div>
+    </div>
   )
 }
 
